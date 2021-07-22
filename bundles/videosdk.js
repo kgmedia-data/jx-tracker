feeb7595a0ad8693562c36f3da40d3ba9b1cec69
jxdeveloper1 <renee@jixie.io>
2021-07-22 13:53:36 +0800

    added hlserror event

--- bundles/videosdk.js
+++ bundles/videosdk.js
@@ -60,7 +60,7 @@ const mids                              = require('../components/basic/ids');
 const createObject                      = require('../components/video/damplayer');
 
 const pginfo = mpginfo.get(); //basic pginfo we can get from the page.
-const dbgVersion = 'v42';
+const dbgVersion = 'v43';
 pginfo.dbgVersion = dbgVersion;
 
 var instMap = new Map();   
