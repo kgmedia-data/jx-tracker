16c9dbad5bb7f888691737de91a456339f559129
jxdeveloper1 <renee@jixie.io>
2021-07-27 07:11:51 +0800

    emergency fix for amp from amp cache uncaught exception

--- bundles/videosdk.js
+++ bundles/videosdk.js
@@ -60,7 +60,7 @@ const mids                              = require('../components/basic/ids');
 const createObject                      = require('../components/video/damplayer');
 
 const pginfo = mpginfo.get(); //basic pginfo we can get from the page.
-const dbgVersion = 'v43';
+const dbgVersion = 'v44';
 pginfo.dbgVersion = dbgVersion;
 
 var instMap = new Map();   
