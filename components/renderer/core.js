777a616a84e408559d14438286c3483d70338f2d
Renee Woo <renee@jixie.io>
2023-01-26 09:23:43 +1100

    moved the decision of the load pixel url to getNOrmalizedCreative function

--- components/renderer/core.js
+++ components/renderer/core.js
@@ -1226,21 +1226,15 @@ MakeOneFloatingUnit = function(container, params, divObjs, dismissCB, univmgr) {
         let jxbnFixedDiv = common.newDiv(jxbnScaleDiv, 'div', null, null, 'jxbf_' + id);
         let jxCoreElt = null;
 
-        let pixelUrl = 'https://traid.jixie.io/sync/ad?action=load&source=outstream';
-        if (normCrParams.trackers) {
-            pixelUrl = normCrParams.trackers.baseurl + '?' + normCrParams.trackers.parameters + '&action=load';
-        } else if (adP && adP.trackers) {
-            pixelUrl = adP.trackers.baseurl + '?' + adP.trackers.parameters + '&action=load';
+        if (normCrParams.loadtrackerurl) {
+            let pixelElt = document.createElement('img');
+            pixelElt.src = normCrParams.loadtrackerurl;
+            pixelElt.style.cssText = 'width:1px;height:1px;';
+
+            let jxPixelDiv = common.newDiv(jxmasterDiv, 'div', null, null, 'jxp_' + id);
+            jxPixelDiv.style.cssText = 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);height:1px;';
+            jxPixelDiv.appendChild(pixelElt);
         }
-
-        let pixelElt = document.createElement('img');
-        pixelElt.src = pixelUrl;
-        pixelElt.style.cssText = 'width:1px;height:1px;';
-
-        let jxPixelDiv = common.newDiv(jxmasterDiv, 'div', null, null, 'jxp_' + id);
-        jxPixelDiv.style.cssText = 'position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);height:1px;';
-        jxPixelDiv.appendChild(pixelElt);
-
         jxmasterDiv.style.position = 'relative';
         jxmasterDiv.style.margin = 'auto';
         jxbnDiv.style.position = 'relative';
@@ -1997,9 +1991,11 @@ const thresholdDiff_ = 120;
         let sendTrackerActions = null; 
         let trackers = c.trackers ? c.trackers: ( c.adparameters.trackers ? c.adparameters.trackers: null);
         let clicktrackerurl = null;
+        let loadtrackerurl = null;
         if (trackers) {
             //need for universal mgr init:
             clicktrackerurl = trackers.baseurl + '?' + trackers.parameters + '&action=click';
+            loadtrackerurl = trackers.baseurl + '?' + trackers.parameters + '&action=load';
         }
 
         // Currently the likes of R2B2 they are not properly integrated with our OSM stack, so
@@ -2314,7 +2310,9 @@ const thresholdDiff_ = 120;
             }
             out.trackers = trackers;
         }
-        
+        if (loadtrackerurl) {
+            out.loadtrackerurl = loadtrackerurl;
+        }
         if (c.adparameters && c.adparameters.jxeventssdk)
             out.jxeventssdk = 1;
         
